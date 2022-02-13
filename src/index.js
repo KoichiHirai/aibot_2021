dfc30af72c163c145e862aed8518abf7dc1ec47f
KoichiHirai <i.remember_tt@icloud.com>
2022-02-13 10:32:05 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.send('Hello World');
+});
+
+// app.listen(3000, function(){});
+(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
