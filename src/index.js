1e51c41ee0a2dea171467369b42cc54e4eb92ba0
KoichiHirai <i.remember_tt@icloud.com>
2022-03-13 15:00:06 +0900

    post updated

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req,res){
-    res.json({message:'Hello World'});
+app.post('/', function(req,res){
+    let data = req.body;
+    console.log('req.body: ',  data);
+    res.send('api: OK');
 });
 
-app.get('/api/get', function(req,res){
-    res.send("api");
-});
+// app.get('/api/get', function(req,res){
+//     res.send("api");
+// });
 
-app.post('/post',function(req,res){
-    res.send(req.body)
-    console.log(req.body);
-});
+// app.post('/post',function(req,res){
+//     res.send(req.body)
+//     console.log(req.body);
+// });
 
 // app.listen(3000, function(){});
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
