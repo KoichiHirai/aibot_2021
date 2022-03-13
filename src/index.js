d5131db54d552612dc880c955ba4a95d49d740ed
KoichiHirai <i.remember_tt@icloud.com>
2022-03-13 15:21:31 +0900

    post updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/', function(req,res){
     let data = req.body;
     console.log('req.body: ' + data);
-    console.log('id: ' +  data['event'][0]['message']['id']);
+    console.log('id: ' +  data['event']);
     res.send('api: OK');
 });
 
