5e6caf32077387c5725b448e674164e130219555
KoichiHirai <i.remember_tt@icloud.com>
2022-03-13 14:24:18 +0900

    task completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req,res){
-    res.send('Hello World');
+    res.json({message:'Hello World'});
+});
+
+app.get('/api/get', function(req,res){
+    res.send("api");
+});
+
+app.post('/post',function(req,res){
+    res.send(req.body)
+    console.log(req.body);
 });
 
 // app.listen(3000, function(){});
